6121786e648b9b4af05aecc57a264e205fbbf28d
Sumair10 <sumairzahid123@gmail.com>
2024-05-28 12:11:41 +0400

    investor added

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,6 +21,7 @@ const INVESTORS = [
     investmentId: "RCI1705001",
     name: "Mr. Mouadh Ben Ameur",
     nationality: "France",
+    countryCode: 'FR',
     emiratedId: "784-1972-996383-6",
     passportNo: "19FF87012",
     resident: "UAE",
@@ -31,6 +32,91 @@ const INVESTORS = [
     iban: "AE810500000000028785206",
     investedAmoutnt: "100,000 USD",
     startDate: "6th May 2024",
+  },
+  {
+    initialId: 2,
+    investmentId: "RCI1705002",
+    name: "Mr. Karim Achoura",
+    nationality: "Tunisia",
+    countryCode: 'TN',
+    emiratedId: "784-1977-3060476-2",
+    passportNo: "H393790",
+    resident: "UAE",
+    phone: "+971 50 425 7310 ",
+    email: "Kachoura@yahoo.fr",
+    bank: "EMIRATES NBD",
+    accountNumber: "1104105102301",
+    iban: "AE120260001104105102301",
+    investedAmoutnt: "367,000 AED",
+    startDate: "6th May 2024",
+  },
+  {
+    initialId: 3,
+    investmentId: "RCI1705003",
+    name: "Ms. Karin Barbara Brosch",
+    nationality: "Germany",
+    countryCode: 'DE',
+    emiratedId: "784-1972-6960904-2",
+    passportNo: "C4KNL5LRJ",
+    resident: "UAE",
+    phone: "050 350 7653",
+    email: "Karin.Brosch@gmail.com",
+    bank: "EMIRATES NBD",
+    accountNumber: "0214201053101",
+    iban: "AE360260000214201053101",
+    investedAmoutnt: "200,000 AED",
+    startDate: "10th May 2024",
+  },
+  {
+    initialId: 4,
+    investmentId: "RCI1705004",
+    name: "Mr. Mouadh Ben Ameur",
+    nationality: "France",
+    countryCode: 'FR',
+    emiratedId: "784-1972-996383-6",
+    passportNo: "19FF87012",
+    resident: "UAE",
+    phone: "+243 858 792 703",
+    email: "benameurmouadh@gmail.com",
+    bank: "ADIB bank",
+    accountNumber: "28785206",
+    iban: "AE810500000000028785206",
+    investedAmoutnt: "376,000 AED",
+    startDate: "15th May 2024",
+  },
+  {
+    initialId: 5,
+    investmentId: "RCI1705005",
+    name: "Mr. Karim Achoura",
+    nationality: "Tunisia",
+    countryCode: 'TN',
+    emiratedId: "784-1977-3060476-2",
+    passportNo: "H393790",
+    resident: "UAE",
+    phone: "+971 50 425 7310 ",
+    email: "Kachoura@yahoo.fr",
+    bank: "EMIRATES NBD",
+    accountNumber: "1104105102301",
+    iban: "AE120260001104105102301",
+    investedAmoutnt: "376,000 AED",
+    startDate: "15th May 2024",
+  },
+  {
+    initialId: 6,
+    investmentId: "RCI1705006",
+    name: "Ms. Lynn Kamal El Hariri El Rifai",
+    nationality: "Lebanon",
+    countryCode: 'LB',
+    emiratedId: "784-1997-6977554-7",
+    passportNo: "LR3717958",
+    resident: "UAE",
+    phone: "+971 52 7822 700",
+    email: "haririlynn@gmail.com ",
+    bank: "EMIRATES NBD",
+    accountNumber: "1015784679001",
+    iban: "AE650260001015784679001",
+    investedAmoutnt: "5,000 USD",
+    startDate: "22th May 2024",
   },
 ];
 
@@ -94,15 +180,16 @@ export default function Main() {
             <>
               <Grid item xs={6} sm={4} md={3}>
                 <Item onClick={handleClick}>
-                  <img
+                  {/* <img
                     src={profile2}
                     alt="description_of_your_image"
                     width="150px"
                     height="150px"
-                  />
+                  /> */}
+                  <h3>{investor.investmentId}</h3>
                   <h1>{investor.name}</h1>
                   <ReactCountryFlag
-                    countryCode="TN"
+                    countryCode={investor.countryCode}
                     svg
                     cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/"
                     cdnSuffix="svg"
@@ -111,6 +198,7 @@ export default function Main() {
                       fontSize: "3em",
                     }}
                   />
+                  
                   <Alert
                     sx={{
                       width: {
