99fc8fdea8613f3cd4f03af456fd8c0dee8afe2a
Sumair10 <sumairzahid123@gmail.com>
2024-05-28 15:22:00 +0400

    all added

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,25 +6,29 @@ import pass from "../assets/pass.jpeg";
 import id from "../assets/id.jpg";
 import { Grid, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
+import { INVESTORS } from "./InvestorsData";
 
 export default function Details() {
   const params = useParams();
-  console.log(params);
+
+
   const { id } = params;
-  console.log(id);
+  const investor  = INVESTORS[parseInt(id)-1 ]
+
+  
   return (
     <React.Fragment>
       <Box className="upperBorder"></Box>
       <Box className="detailContainer">
         <Box className="detailNameBox">
 
-          <img
+          {/* <img
             src={profile3}
             alt="description_of_your_image"
             width="150px"
             height="150px"
-          />
-          <h1>Sumair Zahid</h1>
+          /> */}
+          <h1>{investor.name}</h1>
         </Box>
         <Box className="personalInfoContainer">
           <Box className="personalInfoInnerContainer">PERSONAL DETAILS</Box>
@@ -35,28 +39,19 @@ export default function Details() {
               container
             >
               <Grid item xs={4}>
+                <p className="title">Investment ID</p>
+              </Grid>
+              <Grid item xs={8}>
+                <p className="desc">{investor.investmentId}</p>
+              </Grid>
+              <Grid item xs={12}>
+                <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
+              </Grid>
+              <Grid item xs={4}>
                 <p className="title">Full Name</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Sumair Zahid</p>
-              </Grid>
-              <Grid item xs={12}>
-                <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
-              </Grid>
-              <Grid item xs={4}>
-                <p className="title">Email</p>
-              </Grid>
-              <Grid item xs={8}>
-                <p className="desc">sumairzahid123@gmail.com</p>
-              </Grid>
-              <Grid item xs={12}>
-                <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
-              </Grid>
-              <Grid item xs={4}>
-                <p className="title">Phone No.</p>
-              </Grid>
-              <Grid item xs={8}>
-                <p className="desc">+971 56 591 4956</p>
+                <p className="desc">{investor.name}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
@@ -65,79 +60,91 @@ export default function Details() {
                 <p className="title">Nationality</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Pakistan</p>
+                <p className="desc">{investor.nationality}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Age</p>
+                <p className="title">Emirated ID</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">26</p>
+                <p className="desc">{investor.emiratedId}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Job Title</p>
+                <p className="title">Email Address</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">App Developer</p>
+                <p className="desc">{investor.email}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Address</p>
+                <p className="title">Phone No.</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Flat abc, Al-Nadha, Dubai</p>
+                <p className="desc">{investor.phone}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">City</p>
+                <p className="title">Passport No.</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Dubai</p>
+                <p className="desc">{investor.passportNo}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Date of Birth</p>
+                <p className="title">Bank</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">18 December 1999</p>
+                <p className="desc">{investor.bank}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Work Address</p>
+                <p className="title">Account No.</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Suite #1705, Festival Tower, Dubai Festival City</p>
+                <p className="desc">{investor.accountNumber}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Start Date</p>
+                <p className="title">IBAN</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">11 March 2024</p>
+                <p className="desc">{investor.iban}</p>
               </Grid>
               <Grid item xs={12}>
                 <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
               <Grid item xs={4}>
-                <p className="title">Employment Status</p>
+                <p className="title">Agreement Date</p>
               </Grid>
               <Grid item xs={8}>
-                <p className="desc">Full Time</p>
+                <p className="desc">{investor.startDate}</p>
+              </Grid>
+              <Grid item xs={12}>
+                <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
+              </Grid>
+              <Grid item xs={4}>
+                <p className="title">Invested Amount</p>
+              </Grid>
+              <Grid item xs={8}>
+                <p className="desc">{investor.investedAmoutnt}</p>
+              </Grid>
+              <Grid item xs={12}>
+                <Box style={{ borderBottom: '1px solid #e2ecf9' }}></Box>
               </Grid>
 
 
@@ -155,26 +162,24 @@ export default function Details() {
                 container
               >
                 <Grid item xs={12} md={4}  style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', marginTop: '10px' }}>
-                  <img
-                    src={degree}
-                    alt="description_of_your_image"
-                    width="90%"
-                  />
+                <img src={require(`../agreements/00${id}/1.png`)} width="90%"/>
+               
                 </Grid>
                 <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center' , alignItems: 'flex-start', marginTop: '10px' }}>
-                  <img
-                    src={pass}
-                    alt="description_of_your_image"
-                    width="90%"
-                  />
+                <img src={require(`../agreements/00${id}/2.png`)} width="90%"/>
                 </Grid>
                 <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center' , alignItems: 'flex-start', marginTop: '10px' }}>
-                  <img
-                    src={id}
-                    alt="description_of_your_image"
-                    width="90%"
-                  />
+                <img src={require(`../agreements/00${id}/3.png`)} width="90%"/>
                 </Grid>
+                <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center' , alignItems: 'flex-start', marginTop: '10px' }}>
+                <img src={require(`../agreements/00${id}/4.png`)} width="90%"/>
+                </Grid>
+                {
+                  id === '2' || id === '4' ?  <Grid item xs={12} md={4} style={{ display: 'flex', justifyContent: 'center' , alignItems: 'flex-start', marginTop: '10px' }}>
+                  <img src={require(`../agreements/00${id}/5.png`)} width="90%"/>
+                  </Grid>  : ''
+                }
+               
 
               </Grid>
             </Box>
--- src/components/Main.js
+++ src/components/Main.js
@@ -14,111 +14,7 @@ import profile4 from "../assets/p2.jpeg";
 import profile2 from "../assets/p3.png";
 import profile5 from "../assets/p1.jpeg";
 import { useNavigate } from "react-router-dom";
-
-const INVESTORS = [
-  {
-    initialId: 1,
-    investmentId: "RCI1705001",
-    name: "Mr. Mouadh Ben Ameur",
-    nationality: "France",
-    countryCode: 'FR',
-    emiratedId: "784-1972-996383-6",
-    passportNo: "19FF87012",
-    resident: "UAE",
-    phone: "+243 858 792 703",
-    email: "benameurmouadh@gmail.com",
-    bank: "ADIB bank",
-    accountNumber: "28785206",
-    iban: "AE810500000000028785206",
-    investedAmoutnt: "100,000 USD",
-    startDate: "6th May 2024",
-  },
-  {
-    initialId: 2,
-    investmentId: "RCI1705002",
-    name: "Mr. Karim Achoura",
-    nationality: "Tunisia",
-    countryCode: 'TN',
-    emiratedId: "784-1977-3060476-2",
-    passportNo: "H393790",
-    resident: "UAE",
-    phone: "+971 50 425 7310 ",
-    email: "Kachoura@yahoo.fr",
-    bank: "EMIRATES NBD",
-    accountNumber: "1104105102301",
-    iban: "AE120260001104105102301",
-    investedAmoutnt: "367,000 AED",
-    startDate: "6th May 2024",
-  },
-  {
-    initialId: 3,
-    investmentId: "RCI1705003",
-    name: "Ms. Karin Barbara Brosch",
-    nationality: "Germany",
-    countryCode: 'DE',
-    emiratedId: "784-1972-6960904-2",
-    passportNo: "C4KNL5LRJ",
-    resident: "UAE",
-    phone: "050 350 7653",
-    email: "Karin.Brosch@gmail.com",
-    bank: "EMIRATES NBD",
-    accountNumber: "0214201053101",
-    iban: "AE360260000214201053101",
-    investedAmoutnt: "200,000 AED",
-    startDate: "10th May 2024",
-  },
-  {
-    initialId: 4,
-    investmentId: "RCI1705004",
-    name: "Mr. Mouadh Ben Ameur",
-    nationality: "France",
-    countryCode: 'FR',
-    emiratedId: "784-1972-996383-6",
-    passportNo: "19FF87012",
-    resident: "UAE",
-    phone: "+243 858 792 703",
-    email: "benameurmouadh@gmail.com",
-    bank: "ADIB bank",
-    accountNumber: "28785206",
-    iban: "AE810500000000028785206",
-    investedAmoutnt: "376,000 AED",
-    startDate: "15th May 2024",
-  },
-  {
-    initialId: 5,
-    investmentId: "RCI1705005",
-    name: "Mr. Karim Achoura",
-    nationality: "Tunisia",
-    countryCode: 'TN',
-    emiratedId: "784-1977-3060476-2",
-    passportNo: "H393790",
-    resident: "UAE",
-    phone: "+971 50 425 7310 ",
-    email: "Kachoura@yahoo.fr",
-    bank: "EMIRATES NBD",
-    accountNumber: "1104105102301",
-    iban: "AE120260001104105102301",
-    investedAmoutnt: "376,000 AED",
-    startDate: "15th May 2024",
-  },
-  {
-    initialId: 6,
-    investmentId: "RCI1705006",
-    name: "Ms. Lynn Kamal El Hariri El Rifai",
-    nationality: "Lebanon",
-    countryCode: 'LB',
-    emiratedId: "784-1997-6977554-7",
-    passportNo: "LR3717958",
-    resident: "UAE",
-    phone: "+971 52 7822 700",
-    email: "haririlynn@gmail.com ",
-    bank: "EMIRATES NBD",
-    accountNumber: "1015784679001",
-    iban: "AE650260001015784679001",
-    investedAmoutnt: "5,000 USD",
-    startDate: "22th May 2024",
-  },
-];
+import { INVESTORS } from "./InvestorsData";
 
 export default function Main() {
   const navigation = useNavigate();
@@ -150,9 +46,8 @@ export default function Main() {
     },
   }));
 
-  const handleClick = () => {
-    const someId = 1;
-    navigation(`/details/${someId}`);
+  const handleClick = (id) => {
+    navigation(`/details/${id}`);
   };
 
   return (
@@ -179,7 +74,7 @@ export default function Main() {
           {INVESTORS.map((investor) => (
             <>
               <Grid item xs={6} sm={4} md={3}>
-                <Item onClick={handleClick}>
+                <Item onClick={()=>handleClick(investor.initialId)}>
                   {/* <img
                     src={profile2}
                     alt="description_of_your_image"
